c56fd09a10ea10a070293952bcd0c48cbbb2e76a
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-04-04 23:50:06 +0530

    Time: 25 ms (6.00%) | Memory: 14 MB (68.59%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.cpp
+++ 1168-duplicate-zeros/duplicate-zeros.cpp
@@ -1,12 +1,37 @@
 class Solution {
 public:
+    void shift(vector<int>& arr, int i){
+        int n=arr.size();
+        for(int j=n-1;j>i;j--){
+            arr[j]=arr[j-1];
+        }
+    }
+    void duplicateZeros(vector<int>& arr) {
+        int n=arr.size();
+        int i=0;
+        while(i<n){
+            if (arr[i]==0){
+                shift(arr,i);
+                i+=2;
+            }
+            else{
+                i++;
+            }
+        }
+        
+    }
+};
+
+
+/* CODE USING EXTRA SPACE
+Approach - two pointers in same direction (one incrementing by  1 and another by 2)
+
+class Solution {
+public:
     void duplicateZeros(vector<int>& arr) {
         int n=arr.size();
         vector<int>ans;
-        int count=0;
-        for(int i=0; i<n;i++){
-            if(arr[i]==0)count++;
-        }
+        
         int j=0, i=0;
         while(j<n){
             if(arr[i]==0){
@@ -26,3 +51,9 @@ public:
         }
     }
 };
+
+
+Time → O(n) (good)
+Space → O(n) (extra array ans)
+Expects inplace modification 
+*/
