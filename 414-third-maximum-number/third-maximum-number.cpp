5ad1ad6ca59d9238fc095997d7dc064155713b6e
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-04-04 23:31:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (74.78%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,14 +1,57 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        unordered_set<int> s(nums.begin(), nums.end());
-        priority_queue<int> pq(s.begin(), s.end());
-
-        if(pq.size() < 3)
-            return pq.top();   // return max directly
-
-        pq.pop(); // 1st max
-        pq.pop(); // 2nd max
-        return pq.top(); // 3rd max
+        int n=nums.size();
+        long first = LONG_MIN;
+        long sec = LONG_MIN;
+        long third = LONG_MIN;
+        
+        for(int i:nums){
+            if(i>first ){
+                third=sec;
+                sec=first;
+                first=i;
+            }
+            else if(i<first && i>sec  ){
+                third=sec;
+                sec=i;
+            }
+            else{
+                if(i<sec && i>third)
+                third=i;
+            }
+        }
+        if(third==LONG_MIN)return first;
+        return third;
     }
 };
+
+
+
+
+/* USING PRIORITY QUEUE AND UNORDERED SET
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        unordered_set<int> s;      // unordered_set<int> s(nums.begin(), nums.end());
+        for(int i:nums){
+            s.insert(i);
+        }
+        priority_queue<int> pq;    //priority_queue<int> pq(s.begin(), s.end());
+        for(int i:s){
+            pq.push(i);
+        }
+        int size=pq.size();
+        if(size<3){
+            return pq.top();  //max element returned directly
+        }
+
+        pq.pop();
+        pq.pop();
+        return pq.top();  // 3rd max
+    }
+};
+
+TC - O(nlog n)
+SC-O(n)
+*/
