cd0710e842957df5d26a7a6f1e303102cbdafd4d
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-04-05 23:42:04 +0530

    Time: 0 ms (100.00%) | Memory: 23.3 MB (78.59%) - LeetSync

--- 2350-find-closest-number-to-zero/find-closest-number-to-zero.cpp
+++ 2350-find-closest-number-to-zero/find-closest-number-to-zero.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int findClosestNumber(vector<int>& nums) {
+        int ans=nums[0];
+        for(int i:nums){
+            if(abs(i)<abs(ans) || (abs(i)==abs(ans) && i>ans)){
+                ans=i;
+            }
+        }
+        return ans;
+    }
+};
+
+
+/* MY LOGIC 
+class Solution {
+public:
+    int findClosestNumber(vector<int>& nums) {
         int n=nums.size();
         int small=INT_MAX;
         int j=INT_MIN;
@@ -17,5 +32,5 @@ public:
         return j;
     }
 };
-
+*/
 //else if can be used without else
