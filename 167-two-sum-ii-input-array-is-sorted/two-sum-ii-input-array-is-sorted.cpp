4e6c4bb63240f64e7f61c9b0495b503ad902a992
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-04-09 16:33:16 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (64.22%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -4,7 +4,7 @@ public:
         int n=numbers.size();
         int j=n-1;
         int i=0;
-        while(i<=j){
+        while(i<j){
             int need = target-numbers[i];
             if(need==numbers[j]) return {i+1,j+1};
             else if(need<numbers[j]){
@@ -17,3 +17,11 @@ public:
         return {};
     }
 };
+
+/*You used the two-pointer techniqu.(as sorted array)
+Complexity
+Time: O(n)
+Each pointer moves at most n times.
+Space: O(1)
+No extra data structures used.
+*/
