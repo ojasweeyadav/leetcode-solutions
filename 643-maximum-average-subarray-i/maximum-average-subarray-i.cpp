3c5f382701f166a46534681fed084816a56187ca
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-04-12 16:05:03 +0530

    Time: 4 ms (25.24%) | Memory: 113.8 MB (27.27%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -1,15 +1,41 @@
 class Solution {
 public:
+// USING SLIDING WINDOW(TWO POINTER) - but here we calculate max sum first and retufrn its avg
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n=nums.size();
+        int maxSum;
+        int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+        maxSum=sum;
+        int j=0;
+        for(int i=k;i<n;i++){
+            sum+=nums[i];
+            sum-=nums[j];
+            j++;
+            maxSum=max(maxSum,sum);
+        }
+        return (double)maxSum/k;
+    }
+};
+
+
+/*THIS IS 1 APPROACH - USING SLIDING WINDOW(TWO POINTER)
+(in this we directly calculate the avg and return)
+
+class Solution {
+public:
     double findMaxAverage(vector<int>& nums, int k) {
         int n=nums.size();
         double avg;
-        double max_avg=INT_MIN;
+        double max_avg;
         int sum=0;
         for(int i=0;i<k;i++){
             sum+=nums[i];
         }
         avg=(double)sum/k;
-        max_avg=max(max_avg,avg);
+        max_avg=avg;
         int j=0;
         for(int i=k;i<n;i++){
             sum+=nums[i];
@@ -21,3 +47,4 @@ public:
         return max_avg;
     }
 };
+*/
