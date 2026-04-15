ba805517d8d858aced6cd1495dfd2ec8bff98a3e
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-04-15 15:13:17 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (58.41%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+//using single pass
+    int maxProduct(vector<int>& nums) {
+        int n=nums.size();
+        int max1=INT_MIN;
+        int max2=INT_MIN;
+
+        for(int x : nums){
+            if(x>max1){
+                max2=max1;    // shift down
+                max1=x;
+            }
+            else if(x>max2) {
+                max2=x;
+            }
+        }   
+        
+        return (max1-1)*(max2-1);
+    }
+};
+
+
+
+/* TIME - O(N) STILL NOT VERY OPTIMAL AS USES 2 PASSES
+
+class Solution {
+public:
     int maxProduct(vector<int>& nums) {
         int n=nums.size();
         int max1=INT_MIN;
@@ -17,3 +43,4 @@ public:
         return (max1-1)*(max2-1);
     }
 };
+*/
