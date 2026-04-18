e08244f310cfa79b6b89c1ae431f67c91a1dcd59
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-04-18 15:02:31 +0530

    Time: 3 ms (4.49%) | Memory: 8.4 MB (99.23%) - LeetSync

--- 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -3,8 +3,8 @@ public:
     int reverse(int n){
         int rev=0;
         while(n>0){
-            rev=rev*10+(n%10);
-            n=n/10;
+            rev=rev*10+(n%10);     //reverse = reverse × 10 + last_digit
+            n=n/10;                //number = number ÷ 10
         }
         return rev;
     }
