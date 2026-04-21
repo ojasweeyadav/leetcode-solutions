bc46332556e4ca807f92977cf80347652f75bfc1
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-04-21 18:31:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (32.67%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
-//Memorization(top-down)
+//Tabulation (Bottom-up)
+    int climb(int n){
+        if(n<=2)return n;
+        vector<int>dp(n+1);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    int climbStairs(int n) {
+        return climb(n);
+    }
+};
+
+/* 
+class Solution {
+public:
+//**Memorization(top-down)**
     int climb(int n, vector<int>&dp){
         if(n==2 || n==1)return n;
         if(dp[n]!=-1)return dp[n];
@@ -12,3 +31,4 @@ public:
         return climb(n,dp);
     }
 };
+*/
