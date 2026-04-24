97dbe463d57a5da550f7adfbfd4c2ae05f4ee564
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-04-25 00:21:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (73.98%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
+        vector<int>prevRow;
+        for(int i=0;i<=rowIndex;i++){
+            vector<int>currentRow(i+1,1);
+            for(int j=1;j<i;j++){
+                currentRow[j]=prevRow[j]+prevRow[j-1];
+            }
+            prevRow=currentRow;
+        }
+        return prevRow;
+    }
+};//O(n):SC
+
+/* SAME EXACT AS PASCAL TRIANGLE 1 BUT THEY WANT 1D ARRAY
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
         vector<vector<int>>dp;
         for(int i=0;i<=rowIndex;i++){
             vector<int>row(i+1,1);
@@ -12,3 +28,4 @@ public:
         return dp[rowIndex];
     }
 };
+*/
