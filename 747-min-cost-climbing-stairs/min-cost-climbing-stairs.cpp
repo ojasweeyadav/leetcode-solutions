054acac61e624ea4625c75e081d00a8fcbab789f
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-04-24 23:37:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (49.11%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//Better version(cleaner)
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
         vector<int>dp(n+1);
@@ -7,8 +8,28 @@ public:
 
         //base cases
         dp[n-1]=cost[n-1]; //take 1 step
-        // dp[n]=0; //already at top
-        dp[n-2]=cost[n-2];
+        dp[n]=0; //already at top
+        
+
+        for(int i=n-2;i>=0;i--){
+            dp[i]=cost[i]+min(dp[i+1],dp[i+2]);
+        }
+        return min(dp[0],dp[1]);
+    }
+};
+
+/*MY VERSION
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        vector<int>dp(n);
+        //dp[i]=min cost to reach top from ith index
+
+        //base cases
+        dp[n-1]=cost[n-1]; //take 1 step
+        dp[n-2]=cost[n-2]; //take 2 step
 
         for(int i=n-3;i>=0;i--){
             dp[i]=cost[i]+min(dp[i+1],dp[i+2]);
@@ -16,3 +37,4 @@ public:
         return min(dp[0],dp[1]);
     }
 };
+*/
