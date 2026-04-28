f9e4a0dbc649049ec43ec15a9e7cbe0b4bfe0b9b
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-04-28 18:55:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (8.63%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -1,7 +1,34 @@
 class Solution {
 public:
     bool divisorGame(int n) {
+        vector<bool>dp(n+1);
+        dp[1]=false;
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<i;j++){
+                if(i%j==0){
+                    if(dp[i-j]==false){
+                        dp[i]=true;
+                        break;
+                    }
+                    else{
+                        dp[i]=false;
+                    }
+                }
+            }
+        }
+        return dp[n];
+    }
+};
+
+
+/* OPTIMAL 
+class Solution {
+public:
+    bool divisorGame(int n) {
         if(n%2==0)return true;
         else return false;
     }
 };
+*/
+
+//see Note
