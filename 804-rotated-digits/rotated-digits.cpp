d27cb49f44ce0c75b63460050bfda4e7575f7104
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-05-02 15:05:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (22.81%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     int rotatedDigits(int n) {
         vector<int>dp(n+1,0);
+        int count=0;
         for(int i=0;i<=n;i++){         //i=(i/10)*10+(i%10);
             if(i<10){
                 if(i==3 || i==4 || i==7)dp[i]=0; // invalid
                 else if(i==0 || i==1 || i==8)dp[i]=1; //valid but same
                 else{
                     dp[i]=2; //valid and different(for 2,5,6,9)
+                    count++; //to avoid the extra count loop
                 } 
             }
             else{
@@ -17,14 +19,14 @@ public:
                 else if(a==1 && b==1)dp[i]=1;
                 else{
                     dp[i]=2;
+                    count++; //to avoid the extra count loop
                 }
 
             }
         }
-        int count=0;
-        for(int i:dp){
-            if(i==2)count++;
-        }
+        // for(int i:dp){
+        //     if(i==2)count++;
+        // }
         return count;
     }
 };
