04f0e09d12907b7d4f51ebe4c553b477be84daff
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-05-06 23:33:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (7.64%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,3 +1,4 @@
+//2D DP
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
@@ -21,6 +22,49 @@ public:
         for(int i=0;i<n;i++){
             min_val=min(min_val,dp[n-1][i]);
         }
-        return min_val;
+        return min_val;   //return *min_element(dp[n-1].begin(), dp[n-1].end());
     }
 };
+//
+
+/*DP APPROACH IN MY TERMS(2D) -
+
+here we took : dp[i][j] = minimum path sum to reach triangle[i][j]
+
+so we calculated dp[i][j] for all possible values and taking min of last row
+
+(No greedy thinking anymore
+You’re no longer doing: “pick best next”
+Instead: “compute best for all”
+That’s the core DP mindset.)
+*/
+
+
+
+/* USING 1D AS ASKED BY QUES
+class Solution {
+public:
+//USING 1D - DP
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> dp(n, 0);
+
+        dp[0] = triangle[0][0];
+
+        for (int i = 1; i < n; i++) {
+            // rightmost element (must come from diagonal)
+            dp[i] = dp[i-1] + triangle[i][i];
+
+            // middle elements (right → left)
+            for (int j = i - 1; j > 0; j--) {
+                dp[j] = min(dp[j], dp[j-1]) + triangle[i][j];
+            }
+
+            // leftmost element (same column) - for Row 1
+            dp[0] = dp[0] + triangle[i][0];
+        }
+
+        return *min_element(dp.begin(), dp.begin() + n);
+    }
+};
+*/
