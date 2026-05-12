3abba099265c3e1a7fb6efc0376aed74284509a2
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-05-12 23:46:09 +0530

    Time: 10 ms (19.87%) | Memory: 16.1 MB (18.23%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    void seperate(vector<int>&ans,int x){
+//My Approach(using MATH) - BOTH HAVE SAME TC AND SC
+    void separate(vector<int>&ans,int x){
         vector<int>rev;
         while(x>0){
             int dig=x%10;
@@ -19,9 +20,29 @@ public:
                 ans.push_back(i);
             }
             else{
-                seperate(ans,i);
+                separate(ans,i);
             }
         }
         return ans;
     }
 };
+//
+
+/*
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> ans;
+
+        for(int num : nums){
+            string s = to_string(num);  //12-integer "12"-string After conversion: you can loop through digits as characters.
+
+            for(char c : s){
+                ans.push_back(c - '0');  //c-'0' => character digit becomes integer digit.
+            }
+        }
+
+        return ans;
+    }
+};
+*/
