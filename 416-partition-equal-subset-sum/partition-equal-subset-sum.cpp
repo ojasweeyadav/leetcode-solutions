8f5744eca8f786666d144db4f198899f74bb0b6a
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-05-13 01:50:21 +0530

    Time: 145 ms (36.69%) | Memory: 17.4 MB (62.79%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,74 @@
+// class Solution {
+// public:
+//     bool canPartition(vector<int>& nums) {
+//         int n=nums.size();
+//         int sum=0;
+//         for(int i:nums){
+//             sum+=i;
+//         }
+//         if(sum%2!=0)return false;
+//         int target=sum/2;
+//         vector<vector<bool>>dp(n,vector<bool>(target+1));
+//         //dp[i]=including this eelement will get us target sum or not
+
+//         dp[]
+
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<=target;j++){
+
+//             }
+//         }
+//     }
+// };
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+
+        int sum = 0;
+        for(int x : nums){
+            sum += x;
+        }
+
+        if(sum % 2 != 0) return false;
+
+        int target = sum / 2;
+
+        vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
+
+        // sum 0 is always possible
+        for(int i = 0; i < n; i++){
+            dp[i][0] = true;
+        }
+
+        // first element alone can form nums[0]
+        if(nums[0] <= target){
+            dp[0][nums[0]] = true;
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j <= target; j++){
+
+                bool notTake = dp[i-1][j];
+
+                bool take = false;
+                if(j >= nums[i]){
+                    take = dp[i-1][j - nums[i]];
+                }
+
+                dp[i][j] = take || notTake;
+            }
+        }
+
+        return dp[n-1][target];
+    }
+};
+
+//main target - Can we find a subset with sum = totalSum / 2 ?
+
+/*APPROACH -
+If one subset reaches target sum/2,
+the remaining elements automatically form the other subset.
+
+So the problem simplifies to:
+Can I pick some elements whose sum becomes target?*/
