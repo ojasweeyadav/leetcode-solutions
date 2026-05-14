2e4e70434b57cb4bff8f640368c604d0e143266d
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-05-15 03:05:44 +0530

    Time: 199 ms (44.19%) | Memory: 178.6 MB (54.45%) - LeetSync

---
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
@@ -0,0 +1,44 @@
+//node = u and it=v
+class Solution {
+public:
+    int timer=0;
+    void dfs(int node, int parent, vector<int>&vis, vector<int> adj[], vector<int>&tin, vector<int>&low, vector<vector<int>>&bridges){
+        vis[node]=1;
+        tin[node]=low[node]=timer++;
+
+        for(auto it:adj[node]){
+            if(it==parent){
+                continue;
+            }
+            else if(vis[it]){
+                low[node]=min(low[node],tin[it]);
+            }
+            else{
+                dfs(it,node,vis,adj,tin,low,bridges);
+                low[node]=min(low[node],low[it]);
+
+                if(low[it]>tin[node]){
+                    bridges.push_back({node,it});
+                }
+            }
+        }
+    }
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        vector<int> adj[n];
+        for(auto it:connections){
+            adj[it[0]].push_back(it[1]); //as connection is in form of array of two elements
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int>vis(n,0);
+        vector<int>tin(n,0);
+        vector<int>low(n,0);
+        vector<vector<int>>bridges;
+        
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,-1,vis,adj,tin,low,bridges);
+            }
+        }
+        return bridges;
+    }
+};
