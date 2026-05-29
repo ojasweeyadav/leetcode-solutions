af499d3f0f02c6a1412fbfc2ba55512f1420e9c4
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-05-29 18:51:06 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (6.96%) - LeetSync

---
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    int digsum(int n){
+        int dig;
+        int sum=0;
+        while(n>0){
+            dig=n%10;
+            n/=10;
+            sum+=dig;
+        }
+        return sum;
+    }
+    int minElement(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            ans[i]=digsum(nums[i]);
+        }
+        return *min_element(ans.begin(),ans.end());
+    }
+};
