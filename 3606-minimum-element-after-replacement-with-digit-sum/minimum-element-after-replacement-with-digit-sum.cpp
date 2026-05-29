d6c357002a196797d2382bfbdcda8c6c614646df
Ojaswee Yadav <yadavojaswee31@gmail.com>
2026-05-29 18:54:05 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (77.43%) - LeetSync

--- 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.cpp
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.cpp
@@ -12,6 +12,30 @@ public:
         return sum;
     }
     int minElement(vector<int>& nums) {
+        int mini=INT_MAX;
+        for(int i:nums){
+            mini=min(mini,digsum(i));
+        }
+        return mini;
+    }
+};
+
+/*MY APPROACH - USING EXTRA (ANS) ARRAY 
+
+class Solution {
+public:
+
+    int digsum(int n){
+        int dig;
+        int sum=0;
+        while(n>0){
+            dig=n%10;
+            n/=10;
+            sum+=dig;
+        }
+        return sum;
+    }
+    int minElement(vector<int>& nums) {
         int n=nums.size();
         vector<int>ans(n);
         for(int i=0;i<n;i++){
@@ -20,3 +44,4 @@ public:
         return *min_element(ans.begin(),ans.end());
     }
 };
+*/
